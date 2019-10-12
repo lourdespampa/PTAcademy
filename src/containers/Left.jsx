e330667a24ea8914e8aadb01c867b17e00839d09
Mirian Orellana <miryan.orellana@playecgroup.com>
2019-10-12 11:57:51 -0500

    add left and list

--- src/containers/Left.jsx
+++ src/containers/Left.jsx
@@ -2,11 +2,17 @@ import React from 'react';
 import MenuLeft from './../components/left/MenuLeft';
 
 class Left extends React.Component{
-
-
+async aparecer(){
+    var soc = document.getElementById('social')
+        soc.style.display = "block";
+}
+async desaparecer(){
+    var soc = document.getElementById('social')
+        soc.style.display = "none";
+}
     render(){
         return(
-            <MenuLeft />
+            <MenuLeft aparecer={this.aparecer} desaparecer={this.desaparecer} />
         )
     }
 }
