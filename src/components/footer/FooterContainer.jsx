c8e7a7655216f500eb242323995e04c280e8df57
L3ANDROID <40245178+L3ANDROID@users.noreply.github.com>
2019-10-26 09:58:21 -0500

    cambios v3

--- src/components/footer/FooterContainer.jsx
+++ src/components/footer/FooterContainer.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 
 import './FooterContainer.css';
 
-function FooterContainer(){
+function FooterContainer(props){
 
     return(
         <>
             <footer className="footer-distributed">
-                <div className="footer-left column" onmouseout="DesactivarPres()" onmouseover="ActivarPres()">
+                <div className="footer-left column">
                     <a id="btn-abrir-popup" className="btn-abrir-popup" />
-                        <div className="envoltura" style={{width: "23%", height: "90%", position: "absolute"}}></div>
-                        <iframe class="miniatura" id="diminute" 
+                        {
+                        (props.diapositivaHover)
+                        ?
+                            <div id="menupresentacion" className="footer-left" style={{position: "fixed",width: "23%",height: "18%",border: "solid 5px white",zIndex: 1}} onMouseEnter={props.toggleHoverSlide} onMouseLeave={props.toggleHoverSlide}>
+                                <button className="pres" id="btn-abrir-popup">DIAPOSITIVAS</button>
+                                <button className="pres">FORMULARIO</button>
+                                <button className="pres">VIDEO</button>
+                            </div>
+                        :
+                            <div className="envoltura" style={{width: "23%", height: "90%", position: "absolute"}} onMouseEnter={props.toggleHoverSlide} onMouseLeave={props.toggleHoverSlide}></div>
+                        }
+                        <iframe className="miniatura" id="diminute" 
                             src="https://docs.google.com/presentation/d/e/2PACX-1vRZDJOxRieHaBo7oXEkyCVkdUha4GolZACvw6skP17Spvsty8SD6tG3zcmfZSphXBVxW5rNuklMQYtw/embed?start=false&amp;amp;loop=false&amp;amp;delayms=3000&amp;rm=minimal&amp;slide=id.p1" 
                             width="100%" height="100%" style={{background: "white"}}>
                         </iframe>
@@ -19,12 +29,21 @@ function FooterContainer(){
                         </div> */}
                 </div>
                 <div className="footer-center">
-                    <div class="ml-4 center-container">
+                    <div className="ml-4 center-container">
                         
                     </div>
                 </div>
                 <div className="footer-right">
                     <h3>unknown</h3>
+                </div>
+
+                <div className="overlay" id="overlay">
+                    <div className="popup" id="popup">
+                        <a href="#" id="btn-cerrar-popup" className="btn-cerrar-popup"><i className="fas fa-times"></i></a>
+                        <iframe id="diapo-frame" src="" frameBorder="0" width="960" height="569" style={{width: "100% !important",height: "100%"}} allowFullScreen={true} mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
+                        <div className="btn-back" onclick="backtPpt()"><img width="25" src="/plugin/images/presentation/btn-back.svg" /></div>
+                        <div className="btn-next" onclick="nextPpt()"><img width="25" src="/plugin/images/presentation/btn-next.svg" /></div>
+                    </div>
                 </div>
             </footer>
         </>
--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -3,12 +3,26 @@ import React, { Component } from 'react'
 import FooterContainer from './../components/footer/FooterContainer';
 
 class Footer extends Component {
+    state = {
+        // variable para mostrar u ocultar los 3 botones que aparecen al hacer hover en la diapositiva del footer
+        diapositivaHover: false
+    }
+
+    //FUNCIONES DE FOOTERCONTAINER
+
+    //Funcion para activar o desactivar el estado que se encarga de mostrar los botones de la diapositiva del footer
+    toggleHoverSlide = () => {
+        this.setState({diapositivaHover: !this.state.diapositivaHover})
+    }
 
 
     render() {
         return (
             <div>
-                <FooterContainer />
+                <FooterContainer
+                    // Envio del esatdo y funcion de mostrar los botones de la diapositiva del footer
+                    diapositivaHover={this.state.diapositivaHover} toggleHoverSlide={this.toggleHoverSlide}
+                />
             </div>
         )
     }
