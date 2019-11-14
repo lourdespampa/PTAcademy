1a20d2df1c43f30dea08f200ab12212b65127ea1
Miryan Orellana <miryan.orellana@playtecgroup.com>
2019-11-14 11:15:24 -0500

    v1 temp basic

--- src/pages/Temporizador.jsx
+++ src/pages/Temporizador.jsx
@@ -2,21 +2,48 @@ import React, { Component } from 'react';
 import ProfTemporizador from '../components/temporizador/temporizador'
 
 class Temporizador extends Component {
-  componentDidMount(){
-    
-  }
-  render(){
+  componentDidMount() {
+    this.myInterval = setInterval(() => {
+      const { seconds, minutes } = this.state
 
-    return (
-      <div>
-        <ProfTemporizador/>
-        <div className="container">
-          <input type="text" placeholder="ingresar el tiempo a pasar"/>
-          se van a seguir haciendo pruebas
-        </div>
-      </div>
-    )
+      if (seconds > 0) {
+        this.setState(({ seconds }) => ({
+          seconds: seconds - 1
+        }))
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(this.myInterval)
+        } else {
+          this.setState(({ minutes }) => ({
+            minutes: minutes - 1,
+            seconds: 59
+          }))
+        }
+      }
+    }, 1000)
   }
+
+
+componentWillUnmount() {
+  clearInterval(this.myInterval)
+}
+state = {
+  minutes: 3,
+  seconds: 0
+}
+render(){
+  const { minutes, seconds } = this.state;
+  return (
+    <>
+      <ProfTemporizador />
+      <div className="container">
+        <input type="number" className="input-minutes"/>
+        <h1>Esta en iniciando en {minutes}:{seconds < 10 ? `0${seconds}` : seconds}</h1>
+      </div>
+    </>
+  )
+}
 }
 
 export default Temporizador;
