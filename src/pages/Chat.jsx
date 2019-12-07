4bb1f3af75c7d5f513aeef747675865a2defcee0
Mirian Orellana <miryan.orellana@playecgroup.com>
2019-12-06 21:30:15 -0500

    Initial commit of all files to the repository

---
+++ src/pages/Chat.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+//import Pizarra from './../components/pizarra/Pizarra.jsx'
+//import '../components/pizarra/pizarra';
+//import CHAT from './../components/chat/chat'
+import Test from './../components/chat/test'
+class chat_page extends React.Component {
+
+
+    render(){
+        return(
+         
+            <Test/>
+        )
+    }
+}
+
+export default chat_page;
