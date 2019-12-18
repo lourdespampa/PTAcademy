3bc212d5a9f227cbb52076bc38c60a88a176ae06
Miryan Orellana <miryan.orellana@playtecgroup.com>
2019-12-18 06:42:38 -0500

    cambios

--- src/pages/Grupos.jsx
+++ src/pages/Grupos.jsx
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import gruposAlumno from "../components/grupos/grupos";
-class Grupos extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      students: []
-    };
-  }
-  componentDidMount() {
-    fetch("http://api-playtec.herokuapp.com/v1/api/student/5dc33c64461b7087bd52b506", { method: "GET" })
-      .then(response => response.json())
-      .then(response2 => console.log(response2));
-  }
-  render() {
-    const { students } = this.state;
-    return (
-      <div>
-        <h1>usuarios</h1>
-        <div className="container">
-          {students.map(u => (
-            <gruposAlumno
-              key={u._id}
-              name={u.first_name}
-              username={u.last_name}
-              email={u.username}
-            />
-          ))}
-        </div>
-      </div>
-    );
-  }
-=======
 import React from 'react';
 import GruposPage from '../components/grupos/GrupoPage'
 
@@ -43,6 +9,5 @@ class Grupos extends React.Component {
             </div>
         )
     }
->>>>>>> 9fc83b6c8164d8900210a0b1083c237f469ac69e
 }
 export default Grupos;
--- src/pages/ListaAlumnos.jsx
+++ src/pages/ListaAlumnos.jsx
@@ -7,10 +7,6 @@ class ListaAlumnos extends React.Component {
     render(){
         return(
             <ListaAlum />
-<<<<<<< HEAD
-=======
-
->>>>>>> 9fc83b6c8164d8900210a0b1083c237f469ac69e
         )
     }
 }
