8eaf703832c7bee859e95118682dff8763b38536
Miryan Orellana <miryan.orellana@playtecgroup.com>
2019-12-19 00:11:19 -0500

    falta añadir estilos

--- src/components/temporizador/temporizador.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function temporizador(props){
-    
-    return(
-        <div>en esta seccion va a ir el temporizador</div>
-    )
-}
-export default temporizador;
--- src/pages/teacher/Temporizador.jsx
+++ src/pages/teacher/Temporizador.jsx
@@ -1,8 +1,106 @@
-import React from "react";
-//import TemporizadoProf from '../components/temporizador/temporizador'
-class Temporizador extends React.Component {
+import React, { Component } from "react";
+import moment from "moment";
+import TimerHeader from "../../components/teacher/temporizador/TimerHeader";
+import TimerDisplay from "../../components/teacher/temporizador/TimerDisplay";
+import TimerButton from "../../components/teacher/temporizador/TimerButton";
+import TimerConfig from "../../components/teacher/temporizador/TimerConfig";
+import * as timerStates from "../../components/teacher/temporizador/timerStates";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default class Temporizador extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentTime: moment.duration(25, "minutes"),
+      baseTime: moment.duration(25, "minutes"),
+      timerState: timerStates.NOT_SET,
+      timer: null
+    };
+
+    this.setBaseTime = this.setBaseTime.bind(this);
+    this.startTimer = this.startTimer.bind(this);
+    this.stopTimer = this.stopTimer.bind(this);
+    this.reduceTimer = this.reduceTimer.bind(this);
+  }
+
+  setBaseTime(newBaseTime) {
+    this.setState({
+      baseTime: newBaseTime,
+      currentTime: newBaseTime
+    });
+  }
+
+  startTimer() {
+    this.setState({
+      timerState: timerStates.RUNNING,
+      timer: setInterval(this.reduceTimer, 1000)
+    });
+  }
+
+  stopTimer() {
+    if (this.state.timer) {
+      clearInterval(this.state.timer);
+    }
+
+    this.setState({
+      timerState: timerStates.NOT_SET,
+      timer: null,
+      currentTime: moment.duration(this.state.baseTime)
+    });
+  }
+
+  reduceTimer() {
+    if (
+      this.state.currentTime.get("hours") === 0 &&
+      this.state.currentTime.get("minutes") === 0 &&
+      this.state.currentTime.get("seconds") === 0
+    ) {
+      this.completeTimer();
+      return;
+    }
+
+    const newTime = moment.duration(this.state.currentTime);
+    newTime.subtract(1, "second");
+
+    this.setState({
+      currentTime: newTime
+    });
+  }
+
+  completeTimer() {
+    if (this.state.timer) {
+      clearInterval(this.state.timer);
+    }
+
+    this.setState({
+      timerState: timerStates.COMPLETE,
+      timer: null
+    });
+  }
+
   render() {
-    return <div>temporizador</div>;
+    return (
+      <div className="container-fluid">
+        <TimerHeader />
+        <TimerDisplay
+          currentTime={this.state.currentTime}
+          timerState={this.state.timerState}
+        />
+        <TimerButton
+          startTimer={this.startTimer}
+          stopTimer={this.stopTimer}
+          timerState={this.state.timerState}
+        />
+        <br />
+        <button className="btn btn-info center-block">PAUSAR</button>
+        {this.state.timerState !== timerStates.RUNNING && (
+          <TimerConfig
+            baseTime={this.state.baseTime}
+            setBaseTime={this.setBaseTime}
+          />
+        )}
+      </div>
+    );
   }
 }
-export default Temporizador;
