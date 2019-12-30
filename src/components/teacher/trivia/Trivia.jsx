b2c817d45b969edea14cf24037875ca34928bc36
Lourdes1514 <lourdes.pampa@tecsup.edu.pe>
2019-12-30 15:14:08 -0500

    cambios

--- src/components/teacher/trivia/Trivia.jsx
+++ src/components/teacher/trivia/Trivia.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./trivia.css";
+import "./trivia.sass";
 import "./botones.scss";
 
 import io from 'socket.io-client';
@@ -182,7 +182,7 @@ class Trivia extends React.Component {
       <Container>
         <form>
         <Row>
-            <Col>
+            <Col md="6">
               <div style={{marginTop:"5px"}}>
                 <label className="label" for="pregunta">Pregunta</label>
                 <input type="text" id="pregunta" name="firstname" class="pregunta" value={this.state.pregunta} onChange={this.changeQuestion}/>
@@ -225,7 +225,7 @@ class Trivia extends React.Component {
                 </div>            
               </div>
             </Col>
-            <Col>
+            <Col md="6">
               <div style={{marginTop:"5px"}}> 
                 <div class="containerRight">
                   <label for="fname">Medio de Comunicación</label>
