36598a2fe697f27e524778c762ad5b61938cd4c3
Mirian Orellana <miryan.orellana@playecgroup.com>
2020-01-10 13:05:18 -0500

    cambios

--- src/view/courses/CoursesTeacher.jsx
+++ src/view/courses/CoursesTeacher.jsx
@@ -3,109 +3,118 @@ import NavCourse from "../classAndCourse/NavCourse";
 import AllCourses from "./AllCourses";
 import axios from "axios";
 import "../courses/Course.sass";
-import {Modal} from 'react-bootstrap'
+import { Modal } from "react-bootstrap";
 export default class CoursesTeacher extends Component {
-
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       nombreProfesor: "",
       _id: "",
       id_curso: "",
-      showdelete:false,
+      showdelete: false,
       courses: []
-    }
+    };
   }
 
   componentDidMount() {
-    var varToken = localStorage.getItem('token');
+    var varToken = localStorage.getItem("token");
     //obtenemos el id de la url pasada a través de las propiedades
-    const { match: { params } } = this.props;
-    this.setState({_id: params.id})
+    const {
+      match: { params }
+    } = this.props;
+    this.setState({ _id: params.id });
     //luego, obtenemos la lista de cursos del profesor por petición a la API
     axios({
       url: `${this.props.apiUrl}/v1/api/teacher/${params.id}/course_detail`,
-      method: 'GET',
+      method: "GET",
       headers: {
-        'x-access-token': `${varToken}`
+        "x-access-token": `${varToken}`
       }
-    }).then( ({ data }) => {
-      // console.log(data)
-        if(data == []){
-          this.setState({courses: []})
-        }else{
-          this.setState({courses: data})
+    })
+      .then(({ data }) => {
+        // console.log(data)
+        if (data == []) {
+          this.setState({ courses: [] });
+        } else {
+          this.setState({ courses: data });
         }
-    })
-    .catch( e => console.log(e))
-    axios({url:`${this.props.apiUrl}/v1/api/admin/user/${params.id}`,
-    method:'GET' ,
-    headers:{
-      'x-access-token': `${varToken}`
-    }
-    }).then(({data})=>{
-      console.log(data)
-      this.setState({nombreProfesor:data.displayName})
-    })
+      })
+      .catch(e => console.log(e));
+    axios({
+      url: `${this.props.apiUrl}/v1/api/admin/user/${params.id}`,
+      method: "GET",
+      headers: {
+        "x-access-token": `${varToken}`
+      }
+    }).then(({ data }) => {
+      console.log(data);
+      this.setState({ nombreProfesor: data.displayName });
+    });
   }
 
-  getCursos(){
-    const { match: { params } } = this.props;
-    var varToken = localStorage.getItem('token');
+  getCursos() {
+    const {
+      match: { params }
+    } = this.props;
+    var varToken = localStorage.getItem("token");
     axios({
       url: `${this.props.apiUrl}/v1/api/teacher/${params.id}/course_detail`,
-      method: 'GET',
+      method: "GET",
       headers: {
-        'x-access-token': `${varToken}`
-      }
-    }).then( ({ data }) => {
-      // console.log(data)
-      if(this.finalizarComponente){
-        if(data == []){
-          this.setState({courses: []})
-        }else{
-          this.setState({courses: data})
-        }
+        "x-access-token": `${varToken}`
       }
     })
-    .catch( e => console.log(e))
+      .then(({ data }) => {
+        // console.log(data)
+          if (data == []) {
+            this.setState({ courses: [] });
+          } else {
+            this.setState({ courses: data });
+          }
+      })
+      .catch(e => console.log(e));
   }
 
   deleteCurso = async () => {
-    var varToken = localStorage.getItem('token');
+    var varToken = localStorage.getItem("token");
 
     await axios({
-      url:this.props.apiUrl+'/v1/api/admin/course/'+ this.state.id_curso,
-      method:'delete',
-      headers:{
-        'x-access-token': `${varToken}`
-      }});
+      url: this.props.apiUrl + "/v1/api/admin/course/" + this.state.id_curso,
+      method: "delete",
+      headers: {
+        "x-access-token": `${varToken}`
+      }
+    });
     this.getCursos();
-  }
-  onClick = (id) => {
+  };
+  onClick = id => {
     this.setState({
-      id_curso:id
-    })
-    console.log(id)
-}
-setShow=(nom,val)=>{
-  this.setState({
-          [nom]:val
-   })
-}
+      id_curso: id
+    });
+    console.log(id);
+  };
+  setShow = (nom, val) => {
+    this.setState({
+      [nom]: val
+    });
+  };
   render() {
     return (
       <>
-        <NavCourse apiUrl={this.props.apiUrl} idcourse={this.props.idcourse} idteacher={this.state._id}
-         agregarX={'course'} nombreProfesor={this.state.nombreProfesor} getdata={this.getCursos()}></NavCourse>
+        <NavCourse
+          apiUrl={this.props.apiUrl}
+          idcourse={this.props.idcourse}
+          idteacher={this.state._id}
+          agregarX={"course"}
+          nombreProfesor={this.state.nombreProfesor}
+          getdata={this.getCursos()}
+        ></NavCourse>
         <div className="main">
           <h1>SECCION DE CURSOS</h1>
           <ul className="cards">
-            {
-            this.state.courses.length > 0
-              ?  
+            {this.state.courses.length > 0 ? (
               // <div>true</div>
-              this.state.courses.map((cursos,id) => (
+              this.state.courses.map((cursos, id) => (
                 <li className="cards_item" key={id}>
                   <AllCourses
                     name_course={cursos.course_name}
@@ -117,24 +126,42 @@ setShow=(nom,val)=>{
                     setShow={this.setShow}
                   />
                 </li>
-                ))
-              :
+              ))
+            ) : (
               // <div>false</div>
               <h3>Cargando cursos... Si no tiene, puede crear uno.</h3>
-              }
+            )}
           </ul>
         </div>
-        <Modal size={'SM'} show={this.state.showdelete} onHide={() => this.setShow('showdelete',false)}>
-                <Modal.Header closeButton>
-                    <div className="punto-posi">
-                        <h3 className="punto-text">¿Desea eliminar al Curso?</h3>          
-                    </div>
-                </Modal.Header>
-                <Modal.Body>
-                    <button class="button btnMyM" onClick={() => this.deleteCurso()+this.setShow('showdelete',false)} type="button">si</button> 
-                    <button class="button btnMyM" onClick={() => this.setShow('showdelete',false)} type="button">No</button> 
-                </Modal.Body>
-            </Modal> 
+        <Modal
+          size={"SM"}
+          show={this.state.showdelete}
+          onHide={() => this.setShow("showdelete", false)}
+        >
+          <Modal.Header closeButton>
+            <div className="punto-posi">
+              <h3 className="punto-text">¿Desea eliminar al Curso?</h3>
+            </div>
+          </Modal.Header>
+          <Modal.Body>
+            <button
+              class="button btnMyM"
+              onClick={() =>
+                this.deleteCurso() + this.setShow("showdelete", false)
+              }
+              type="button"
+            >
+              si
+            </button>
+            <button
+              class="button btnMyM"
+              onClick={() => this.setShow("showdelete", false)}
+              type="button"
+            >
+              No
+            </button>
+          </Modal.Body>
+        </Modal>
       </>
     );
   }
