480186e5a74403211be1d3a70d9b206db39b61a2
Jorge Flores <georgeunlimited15@gmail.com>
2020-01-14 10:55:16 -0500

    Commit estable

--- src/components/teacher/header/HeaderCode.jsx
+++ src/components/teacher/header/HeaderCode.jsx
@@ -53,7 +53,6 @@ function HeaderCode(props){
                         <span className="hamburguesa-span"></span>
                         <span className="hamburguesa-span"></span>
                         <span className="hamburguesa-span"></span>
-<<<<<<< HEAD
                         <label className="close-menu" for="checked"/>
                             <ul className="menu-header" id="menu-header"> 
                                 <li><Link onClick={closeMenu} className="lista" to={`/teacher/${props.id_class}/${props.id_access}`}>LISTA</Link></li> 
@@ -62,15 +61,6 @@ function HeaderCode(props){
                                 <li><Link onClick={closeMenu} className="tempor" to={`/teacher/${props.id_class}/${props.id_access}/temporizador`}>TEMPORIZADOR</Link></li>
                                 <li><Link onClick={closeMenu} className="trivia" to={`/teacher/${props.id_class}/${props.id_access}/trivia`}>TRIVIA</Link></li>
                             </ul>
-=======
-                        <ul id="menu-header">
-                            <li><Link className="lista" to={`/teacher/${props.id_class}/${props.id_access}`}>LISTA DE ALUMNOS</Link></li>    
-                            <li><Link className="azar"   to={`/teacher/${props.id_class}/${props.id_access}/azar`}>AL AZAR</Link></li>
-                            <li><Link className="grupos" to={`/teacher/${props.id_class}/${props.id_access}/grupos`}>GRUPOS</Link></li>
-                            <li><Link className="tempor" to={`/teacher/${props.id_class}/${props.id_access}/temporizador`}>TEMPORIZADOR</Link></li>
-                            <li><Link className="trivia" to={`/teacher/${props.id_class}/${props.id_access}/trivia`}>TRIVIA</Link></li>
-                        </ul>
->>>>>>> 2b84553b0ff10a18c137ed8be9e986aef6f41a7c
                 </div>
             </nav>
             <div className="content">
