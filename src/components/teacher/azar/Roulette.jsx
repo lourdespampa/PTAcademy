1f7887c951f2b2d4aba4b3bd8eaf173a35dcbcb9
L3ANDROID <40245178+L3ANDROID@users.noreply.github.com>
2020-01-15 10:14:41 -0500

    agregado estilos

--- src/components/teacher/azar/Roulette.jsx
+++ src/components/teacher/azar/Roulette.jsx
@@ -189,7 +189,7 @@ class Roulette extends React.Component {
     return (
       <div className="roulette">
         <div className="roulette-container">
-          <canvas ref="canvas" className="roulette-canvas"></canvas>
+          <canvas ref="canvas" className="roulette-canvas" width={baseSize*2} height={baseSize*2}></canvas>
         </div>
         <div className="roulette-container2">
           <input type="button" value="spin" onClick={this.handleOnClick} className="button" id="spin" />
