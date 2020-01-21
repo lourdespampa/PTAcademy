ffa5021aba106f0479182767efdc02fb6ae34cec
L3ANDROID <40245178+L3ANDROID@users.noreply.github.com>
2020-01-21 16:50:33 -0500

    listo miryan

--- src/view/courses/CoursesTeacher.jsx
+++ src/view/courses/CoursesTeacher.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from "react";
+
 import NavCourse from "../classAndCourse/NavCourse";
 import AllCourses from "./AllCourses";
+
 import axios from "axios";
+
 import "../courses/Course.sass";
 import { Modal } from "react-bootstrap";
+
 export default class CoursesTeacher extends Component {
   constructor(props) {
     super(props);
