309ece590385cc66254db01a4a2bd36a20a82685
Lourdes1514 <lourdes.pampa@tecsup.edu.pe>
2020-01-21 10:09:53 -0500

    404 lista ahora si

--- src/view/404/NotFound.jsx
+++ src/view/404/NotFound.jsx
@@ -9,7 +9,7 @@ export default class NotFound extends Component {
         return (
             <>
                 <div className="layout">
-                    <div class="title">404</div>
+                    <div class="title">404!</div>
                     <div class="text icon_500">La página que está buscando no se encuentra disponible</div>
                     <button className="home-notfound">
                         <Link className="notfound-link" to="/">Volver a la página principal</Link>
