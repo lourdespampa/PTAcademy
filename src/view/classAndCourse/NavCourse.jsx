1410c6a89841723e9955adf13fcd556dfe457c7c
Miryan Orellana <miryan.orellana@playtecgroup.com>
2020-01-31 10:57:54 -0500

    cambios de cristhian

--- src/view/classAndCourse/NavCourse.jsx
+++ src/view/classAndCourse/NavCourse.jsx
@@ -21,10 +21,18 @@ function BotonAgregar(props) {
       <div className="teacherCourses__main-menu__addCourse" onClick={handleShow}>
         Agregar {props.agregarX}
       </div>
-      {/* <Modal className="modal-teacher__general" show={show} onHide={handleClose}>
-        <button className="modal-teacher__general-close" onClick={handleClose}>
-          <img className="button-zoom" src={iconExit} alt="imagen de cerrar modal" />
-        </button>
+      <Modal className="modal-teacher__general" show={show} onHide={handleClose} animation={false}>
+          {
+            activarX
+            ?
+            <button className="modal-teacher__general-close" onClick={handleClose}>
+            <img className="button-zoom" src={iconExit} alt="imagen de cerrar modal" />
+            </button>
+            :
+            <button className="modal-teacher__general-close" onClick={handleClose} disabled>
+            <img className="button-zoom" src={iconExit} alt="imagen de cerrar modal" />
+            </button>
+          }
         <Modal.Header>
           <Modal.Title >Agregando {props.agregarX}</Modal.Title>
         </Modal.Header>
@@ -35,40 +43,20 @@ function BotonAgregar(props) {
           <FormularioClass handleEnableX={()=>setActivarX(true)} handleDisableX={handleDisableX}  apiUrl={props.apiUrl} handleClose={AgregarClick} idteacher={props.idteacher} idcourse={props.idcourse} menuToggleNavbar={props.menuToggleNavbar}/>
           }
         </Modal.Body>
-      </Modal> */}
-      <div id="modal-general_container" className={show === 0 ? "" : show === 1 ? "six" : show === 2 ? "six out" : ""}>
-        <div className="modal-general_background">
-          <div className="modal-general_bg_content">
-            <button className="modal-general_close" onClick={handleClose}>
-              <img className="button-zoom" src={iconExit} alt="imagen de cerrar modal" />
-            </button>
-            <div className="modal-general_container">
-              { (props.agregarX === 'curso')?
-            <FormularioCourse apiUrl={props.apiUrl} handleClose={AgregarClick} idteacher={props.idteacher} idcourse={props.idcourse} />
-            :
-            <FormularioClass apiUrl={props.apiUrl} handleClose={AgregarClick} idteacher={props.idteacher} idcourse={props.idcourse}/>
-            }
-            </div>
-            <svg className="modal-general_svg" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-              <rect x="0" y="0" fill="none" rx="3" ry="3"></rect>
-            </svg>
-          </div>
-        </div>
-      </div>
+      </Modal>
     </>
   );
 }
 function BotonCerrarSesion(props) {
-  const [show, setShow] = useState(0);
-  const handleClose = () => setShow(2);
-  const handleShow = () => setShow(1);
-
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <>
       <div className="teacherCourses__main-menu__LogOut" onClick={handleShow}>
         Cerrar sesion
       </div>
-      {/* <Modal className="modal-teacher__general" show={show} onHide={handleClose} animation={false}>
+      <Modal className="modal-teacher__general" show={show} onHide={handleClose} animation={false}>
           <button className="modal-teacher__general-close" onClick={handleClose}>
             <img className="button-zoom" src={iconExit} alt="imagen de cerrar modal" />
           </button>
@@ -87,34 +75,7 @@ function BotonCerrarSesion(props) {
             </button>
           </ButtonToolbar>
         </Modal.Body>
-      </Modal> */}
-      <div id="modal-general_container" className={show === 0 ? "" : show === 1 ? "six" : show === 2 ? "six out" : ""}>
-        <div className="modal-general_background">
-          <div className="modal-general_bg_content">
-            <button className="modal-general_close" onClick={handleClose}>
-              <img className="button-zoom" src={iconExit} alt="imagen de cerrar modal" />
-            </button>
-            <div className="modal-general_container">
-              <div className="modal-general_container_header">
-                <span>¿DESEA CERRAR SESIÓN?</span>
-              </div>
-              <div className="modal-general_container_body">
-                <button className="modal-body__button yes" onClick={props.cerrarSesion} variant="primary">
-                    <Link style={{textDecoration:"none"}} to="/">
-                      <div className="button-zoom">SI</div>
-                    </Link>
-                </button>
-                <button className="modal-body__button no" onClick={handleClose}>
-                    <div className="button-zoom">NO</div>
-                </button>
-              </div>
-            </div>
-            <svg className="modal-general_svg" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
-              <rect x="0" y="0" fill="none" rx="3" ry="3"></rect>
-            </svg>
-          </div>
-        </div>
-      </div>
+      </Modal>
     </>
   );
 }
