a01076d88555a46d1deef97340319da739e63cc5
Cristhian <cristhian7109@gmail.com>
2020-02-01 11:51:39 -0500

    cambios

--- src/components/teacher/audio/audio.jsx
+++ src/components/teacher/audio/audio.jsx
@@ -1,94 +1,87 @@
 import React, { Component } from 'react';
 import './audio.sass'
 import io from 'socket.io-client';
-
 import iconExit from "../../../img/cerrar.png";
-//import ScarletsMediaPresenter from './SFMediaStream.min.js'
-//import {MediaPresenter} from 'sfmediastream';
 const {MediaPresenter} = require('sfmediastream/dist/SFMediaStream');
 
 export default class Audio extends Component{
-   
-    constructor(props) {
-        super(props);
-        this.state = {
-          socket:null,
-          show:0,
-          txtTransmision:"DESEA TRANSMITIR AUDIO"
-        }
+  constructor(props) {
+      super(props);
+      this.state = {
+        socket:null,
+        show:0,
+        txtTransmision:"DESEA TRANSMITIR AUDIO"
       }
-    handleClose = () => this.setState({show:2});
-    handleShow = () => this.setState({show:1});
-    componentDidMount(){
-        console.log(this.props.socketUrl)
-        console.log(this.props.id_access)
-        
-    var presenterMedia = false;
-    var micon = document.getElementById('btn');
-    var btn = document.getElementById('btn');
-    var micro = document.getElementById('micro');
- 
-    
-//EMISOR
-    const controller = []
-    micon.addEventListener('click', () => {
-        const socket = io(this.props.socketUrl, {
-            query:
-                { pin: this.props.id_access }
-          })
-            socket.emit('onPlay', {
-                txt: "Se inicio una nueva emision de voz.Por favor da click en el boton reproducir"
-            });
-            if (presenterMedia === false) {
-                // Set latency to 100ms (Equal with streamer)
-                presenterMedia = new MediaPresenter({
-                    audio: {
-                        channelCount: 1,
-                        echoCancellation: true
-                    }
-                }, 500);
-                presenterMedia.onRecordingReady = function(packet) {
-                    // Every new client streamer must receive this header buffer data
-                    socket.emit('bufferHeader', packet);
-                    console.log(packet)
-                }
-                presenterMedia.onBufferProcess = function(streamData) {
-                    
-                    socket.emit('stream', streamData);
-                    console.log("2");
-                    console.log(streamData)
-                }
-                presenterMedia.startRecording();
-                controller.push("xd")
-                this.setState({
-                    txtTransmision:'DESEA DEJAR DE TRANSMITIR AUDIO'
-                })
-                this.handleClose()
-                } else{
-                presenterMedia.stopRecording();
-                presenterMedia = false
-                controller.push("xd")
-                console.log('Stop');
-                socket.emit('msj_stop', {
-                    txt: "La transmicion de voz ha finalizado"
-                });
-                this.handleClose()
-                this.setState({
-                    txtTransmision:'DESEA TRANSMITIR AUDIO'
-                })
-            }       
+    }
+  handleClose = () => this.setState({show:2});
+  handleShow = () => this.setState({show:1});
+  componentDidMount(){
+      console.log(this.props.socketUrl)
+      console.log(this.props.id_access)   
+  var presenterMedia = false;
+  var micon = document.getElementById('btn');
+  var btn = document.getElementById('btn');
+  var micro = document.getElementById('micro');
+  //EMISOR
+  const controller = []
+  micon.addEventListener('click', () => {
+      const socket = io(this.props.socketUrl, {
+          query:
+              { pin: this.props.id_access }
         })
-        const i = [];
-        btn.addEventListener('click',()=>{
-        
-            if(i.length%2 === 0){
-        	micro.style.animation ='ripple 1000ms infinite' ;
-          i.push("sx");
-        }else{
-        micro.style.animation ='none';
+          socket.emit('onPlay', {
+              txt: "Se inicio una nueva emision de voz.Por favor da click en el boton reproducir"
+          });
+          if (presenterMedia === false) {
+              // Set latency to 100ms (Equal with streamer)
+              presenterMedia = new MediaPresenter({
+                  audio: {
+                      channelCount: 1,
+                      echoCancellation: true
+                  }
+              }, 500);
+              presenterMedia.onRecordingReady = function(packet) {
+                  // Every new client streamer must receive this header buffer data
+                  socket.emit('bufferHeader', packet);
+                  console.log(packet)
+              }
+              presenterMedia.onBufferProcess = function(streamData) {
+                  
+                  socket.emit('stream', streamData);
+                  console.log("2");
+                  console.log(streamData)
+              }
+              presenterMedia.startRecording();
+              controller.push("xd")
+              this.setState({
+                  txtTransmision:'DESEA DEJAR DE TRANSMITIR AUDIO'
+              })
+              this.handleClose()
+              } else{
+              presenterMedia.stopRecording();
+              presenterMedia = false
+              controller.push("xd")
+              console.log('Stop');
+              socket.emit('msj_stop', {
+                  txt: "La transmicion de voz ha finalizado"
+              });
+              this.handleClose()
+              this.setState({
+                  txtTransmision:'DESEA TRANSMITIR AUDIO'
+              })
+          }       
+      })
+      const i = [];
+      btn.addEventListener('click',()=>{
+      
+          if(i.length%2 === 0){
+      	micro.style.animation ='ripple 1000ms infinite' ;
         i.push("sx");
-        }
-        })
+      }else{
+      micro.style.animation ='none';
+      i.push("sx");
+      }
+      })
     }
     render(){  
         return  (
