6e50530d6ed97707667588fb50190b644478177e
Miryan Orellana <miryan.orellana@playtecgroup.com>
2020-02-07 10:29:42 -0500

    cambios de miryan

--- src/components/teacher/audio/audio.jsx
+++ src/components/teacher/audio/audio.jsx
@@ -82,7 +82,7 @@ export default class Audio extends Component{
     }
     render(){  
         return  <div id="btn" >
-                    <img id="micro" width="30px" height="30px" src={require("../../../img/footer/micro.svg")} />
+                    <img alt="imagen de microfono" id="micro" width="30px" height="30px" src={require("../../../img/footer/micro.svg")} />
                     <span>Audio</span>                
                 </div>
                 
