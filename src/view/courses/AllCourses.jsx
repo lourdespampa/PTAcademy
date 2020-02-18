06220f2aac073ad2cea03474da3136614087ae0a
Lourdes1514 <lourdes.pampa@tecsup.edu.pe>
2020-02-18 16:13:08 -0500

    grupos alumnos activos e innactivos

--- src/view/courses/AllCourses.jsx
+++ src/view/courses/AllCourses.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "./cardCourses.sass";
 import axios from 'axios';
 export default class AllCourses extends Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.state = {
       cursoConAlumnos: true,
@@ -20,7 +20,7 @@ export default class AllCourses extends Component {
         "x-access-token": `${varToken}`
       }
     })
-      .then(({data}) => {
+      .then(({ data }) => {
         console.log(data)
         if (data.length) {
           this.setState({ cursoConAlumnos: true });
@@ -33,7 +33,7 @@ export default class AllCourses extends Component {
 
   render() {
     return (
-      
+
       <>
         <div className="classTeacher-container">
           <div className="classTeacher-card">
@@ -42,20 +42,20 @@ export default class AllCourses extends Component {
                 this.props.onClick(this.props.id) +
                 this.props.setShow("show", 1)}
                 className="courseTeacher__button-delette">
-                    <i className="courseTeacher__img fas fa-trash"></i>
-                </button>
-                <div className={this.state.cursoConAlumnos ? "courseTeacher__containerButton-Edit" : "courseTeacher__containerButton-Edit courseTeacher__animationEdit"}>
-                <Link to={{pathname:`/${this.props.idteacher}/course_detail/${this.props.id}`, state:this.props.competencias}} className="courseTeacher__button-Edit">
-                    <i className="algodemargin courseTeacher__img fas fa-user-plus"></i>
+                <i className="courseTeacher__img fas fa-trash"></i>
+              </button>
+              <div className={this.state.cursoConAlumnos ? "courseTeacher__containerButton-Edit" : "courseTeacher__containerButton-Edit courseTeacher__animationEdit"}>
+                <Link to={{ pathname: `/${this.props.idteacher}/course_detail/${this.props.id}`, state: this.props.competencias }} className="courseTeacher__button-Edit">
+                  <i className="algodemargin courseTeacher__img fas fa-user-plus"></i>
                 </Link>
-                </div>
+              </div>
               <img className="classTeacher-card__image" src={this.props.imageURL || "https://images.unsplash.com/photo-1519999482648-25049ddd37b1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2126&q=80"} alt="" />
-            </div>              
-              <svg className="classTeacher-card__svg" viewBox="0 0 800 500">
+            </div>
+            <svg className="classTeacher-card__svg" viewBox="0 0 800 500">
 
-                <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333"/>
-                <path className="classTeacher-card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" strokeWidth="3" fill="transparent"/>
-              </svg>            
+              <path d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400 L 800 500 L 0 500" stroke="transparent" fill="#333" />
+              <path className="classTeacher-card__line" d="M 0 100 Q 50 200 100 250 Q 250 400 350 300 C 400 250 550 150 650 300 Q 750 450 800 400" stroke="pink" strokeWidth="3" fill="transparent" />
+            </svg>
             <div className="classTeacher-card__content">
               <p className="classTeacher-card__grade">{this.props.grade}</p>&nbsp;&nbsp;
               <p className="classTeacher-card__section">{this.props.section}</p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -65,15 +65,11 @@ export default class AllCourses extends Component {
             </div>
             <Link
               onClick={() => localStorage.setItem("competencias", JSON.stringify(this.props.competencias))}
-              to={`/${this.props.idteacher}/ClassTeacher/${this.props.id}`}
-              className="courseTeacher__buttonEntry"
-            >
-              <label className="courseTeacher__buttonEntry-label">
-                VER CLASES
-              </label>
+              to={`/${this.props.idteacher}/ClassTeacher/${this.props.id}`} className="courseTeacher__buttonEntry">
+              <div className="button-zoom">VER CLASES</div>
             </Link>
           </div>
-        </div>       
+        </div>
       </>
     );
   }
