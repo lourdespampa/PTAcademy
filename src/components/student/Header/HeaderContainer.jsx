c7f38fe1975e278833c1f2e4d089bb5b2426bb5c
Miryan Orellana <miryan.orellana@playtecgroup.com>
2020-02-21 15:38:27 -0500

    merge rosali

--- src/components/student/Header/HeaderContainer.jsx
+++ src/components/student/Header/HeaderContainer.jsx
@@ -1,203 +1,121 @@
-import React, { useState, useEffect } from "react";
-import { Link, Redirect } from "react-router-dom";
-import "./HeaderContainer.css";
-import SocketStream from "../socket/socketStream";
-import axios from "axios";
-import io from "socket.io-client";
+import React, {useState,useEffect} from 'react'
+import {Link, Redirect} from 'react-router-dom'
+import './HeaderContainer.css'
+import SocketStream from '../socket/socketStream'
+import axios from 'axios'
+import io from 'socket.io-client';
 
-const WindowFocusHandler = props => {
-  // User has switched back to the tab
-  const onFocus = () => {
-    console.log("Tab is in focus");
-  };
+const WindowFocusHandler = (props) => {
+    // User has switched back to the tab
+const onFocus = () => {
+    console.log('Tab is in focus');
+  };  
   // User has switched away from the tab (AKA tab is hidden)
   const onBlur = () => {
-    let pin = props.id_access;
-    const socket = io(props.socketUrl, {
-      query: {
-        pin: pin
-      }
-    });
-    socket.emit("tabBlurred", { fullname: props.fullname });
+      let pin = props.id_access
+      const socket = io(props.socketUrl, {
+        query:
+          { 
+            pin: pin
+          }
+        })
+        socket.emit('tabBlurred',{fullname:props.fullname})
 
-    console.log(props.fullname);
-    console.log("Tab is blurred");
+        console.log(props.fullname);
+        console.log('Tab is blurred');
   };
   useEffect(() => {
-    window.addEventListener("focus", onFocus);
-    window.addEventListener("blur", onBlur);
+    window.addEventListener('focus',onFocus);
+    window.addEventListener('blur',onBlur);    
     // Specify how to clean up after this effect:
     return () => {
-      window.removeEventListener("focus", onFocus);
-      window.removeEventListener("blur", onBlur);
+      window.removeEventListener('focus',onFocus);
+      window.removeEventListener('blur',onBlur);
     };
   });
   return <></>;
 };
 export default function HeaderContainer(props) {
-  const [showResponsive, setShowResponsive] = useState(false);
-  // const [redirect,setredirect]=useState(false); //no se esta usando
-  const [trivia, settrivia] = useState(false);
-  const [temporizador, settemporizador] = useState(false);
-  const [Exit, setExit] = useState(false);
-  const reinicio = () => settrivia(false) + settemporizador(false);
+    const [showResponsive, setShowResponsive] = useState(false)
+    const [redirect,setredirect]=useState(false); //no se esta usando
+    const [trivia,settrivia]=useState(false);
+    const [temporizador,settemporizador]=useState(false);
+    const [Exit,setExit]=useState(false);
+    const reinicio=()=>settrivia(false)+settemporizador(false)
 
-  const deleteStudent = async () => {
-    console.log(props.apiUrl, props.id_access, props.id_student);
-    const data3 = {
-      state: "inactive",
-      id_stud: props.id_student
-    };
-    await axios({
-      url: `${props.apiUrl}/v1/api/student/change_state`,
-      data: data3,
-      method: "post"
-    })
-      .then(async res2 => {
-        await localStorage.clear();
-        console.log(res2);
-        const socket = io(props.socketUrl, {
-          query: { pin: props.id_access }
-        });
-        await socket.emit("newAlum");
-        await setExit(true);
-      })
-      .catch(err2 => console.log(err2));
-  };
-  useEffect(() => {
-    const socket = io(props.socketUrl, {
-      query: { pin: props.id_access }
-    });
-
-    if (localStorage.getItem("token")) {
-      console.log("si tiene");
-    } else {
-      console.log("no tiene");
-      setExit(true);
+    const deleteStudent= ()=>{
+        console.log(props.apiUrl,props.id_access,props.id_student)
+        const data3={
+          state:"inactive",
+          id_stud:props.id_student
+        }
+        axios({
+          url: `${props.apiUrl}/v1/api/student/change_state`,
+          data:data3 ,
+          method: "post"
+        }).then((res2)=>{
+            console.log(res2)
+            const socket = io(props.socketUrl, {
+            query:
+                { pin: props.id_access }
+          })
+          socket.emit('newAlum')
+          setExit(true)
+        }).catch(err2=> console.log(err2))
+         
+          
+          
     }
-    socket.on("redirectAlum", data => {
-      console.log("llega redirectAlum");
-      if (data.pin === props.id_access.toUpperCase()) {
-        if (data.page === "trivia") {
-          settrivia(true);
-          settemporizador(false);
-        } else if (data.page === "temporizador") {
-          settemporizador(true);
-          settrivia(false);
-        }
-      }
-    });
-    socket.on("ExitSocket", data => {
-      if (data.pin === props.id_access.toUpperCase()) {
-        localStorage.clear();
-        setExit(true);
-      }
-    });
-    //liSTA
-    socket.on("RemoveStudS", data => {
-      console.log(data);
-      if (data.pin === props.id_access.toUpperCase()) {
-        console.log("REcibe salida");
-        if (data.id === props.id_student) {
-          console.log("REcibe  salida fase 2");
-          localStorage.clear();
-          setExit(true);
-        }
-      }
-    });
-    //END LISTA
-  });
-  const handleNavbarResponsive = () => {
-    setShowResponsive(!showResponsive);
-  };
-  return (
-    <div>
-      <WindowFocusHandler
-        id_access={props.id_access}
-        fullname={props.name + " " + props.lastName}
-        id_student={props.id_student}
-        socketUrl={props.socketUrl}
-      ></WindowFocusHandler>
-      {trivia ? (
-        <Redirect
-          to={`/student/${props.id_student}/${props.id_access}/trivia`}
-        />
-      ) : null}
-      {temporizador ? (
-        <Redirect
-          to={`/student/${props.id_student}/${props.id_access}/temporizador`}
-        />
-      ) : null}
-      {Exit ? <Redirect to={`/`} /> : null}
-      <header className="alumnoH-header ">
-        <div className="alumnoHeader-logo">
-          <Link
-            onClick={reinicio}
-            to={`/student/${props.id_student}/${props.id_access}`}
-            style={{ textDecoration: "none" }}
-          >
-            <h1 className="alumnoHeader-image">PlayTec Academy</h1>
-          </Link>
-        </div>
-        <i
-          className="fas fa-bars alumnoHeader-menu-toggle"
-          onClick={handleNavbarResponsive}
-        ></i>
-        <ul
-          className={
-            showResponsive
-              ? "alumnoHeader-nav showResponsive"
-              : "alumnoHeader-nav"
+
+    useEffect(()=>{
+        const socket = io(props.socketUrl, {
+            query:
+                { pin: props.id_access }
+          })
+        socket.on('redirectAlum', (data) => {
+            console.log('llega redirectAlum')
+            if(data.pin === (props.id_access).toUpperCase()){
+                if(data.page === 'trivia'){
+                    settrivia(true)
+                    settemporizador(false)
+                }else if(data.page === 'temporizador'){
+                    settemporizador(true)
+                    settrivia(false)
+                }
+            }
+        })
+        socket.on('ExitSocket',(data)=>{
+            if(data.pin===(props.id_access).toUpperCase()){
+                setExit(true)
+            }
+        })       
+        //liSTA
+        socket.on('RemoveStudS',(data)=>{
+            console.log(data)
+            if(data.pin === (props.id_access).toUpperCase()) {
+                console.log('REcibe salida')
+                if(data.id===props.id_student){
+                    console.log('REcibe  salida fase 2')
+                    setExit(true)
+                }
+            }
+        })
+        //END LISTA
+    })
+    const handleNavbarResponsive = () => {
+        setShowResponsive(!showResponsive)
+    }
+    return (
+        <div>
+          <WindowFocusHandler id_access={props.id_access} fullname={props.name+' '+props.lastName} id_student={props.id_student} socketUrl={props.socketUrl} ></WindowFocusHandler>
+          {
+          trivia
+          ? <Redirect to={`/student/${props.id_student}/${props.id_access}/trivia`} /> : null
           }
-<<<<<<< HEAD
-        >
-          <li className="alumnoHeader-li">
-            <Link
-              onClick={reinicio}
-              className="alumnoHeader-a"
-              to={`/student/${props.id_student}/${props.id_access}/trivia`}
-            >
-              <i className="fa fa-list-ol"></i>
-              Trivia
-            </Link>
-          </li>
-          <li className="alumnoHeader-li">
-            <Link
-              onClick={reinicio}
-              className="alumnoHeader-a"
-              to={`/student/${props.id_student}/${props.id_access}/temporizador`}
-            >
-              <i
-                className="fas fa-hourglass"
-                style={{ fontSize: "22px", padding: "0 10px" }}
-              ></i>
-              Temporizador
-            </Link>
-          </li>
-          {/* <li className="alumnoHeader-"><a className="alumnoHeader-"></a></li> */}
-          <li className="alumnoHeader-li">
-            <a href className="alumnoHeader-a">
-              <i className="fa fa-user"></i>
-              {props.name} {props.lastName}
-              <i className="fa fa-chevron-down"></i>
-            </a>
-            <ul
-              className={
-                showResponsive
-                  ? "alumnoHeader-nav showResponsive"
-                  : "alumnoHeader-nav"
-              }
-              style={{ zIndex: "5000" }}
-            >
-              <li className="alumnoHeader-li" onClick={() => deleteStudent()}>
-                <a href className="alumnoHeader-a alumnoHeader-salir">
-                  <i
-                    className="fas fa-sign-out-alt"
-                    style={{ fontSize: "22px", padding: "0 10px" }}
-                  ></i>
-                  Salir
-                </a>
-=======
+          {
+          temporizador
+          ? <Redirect to={`/student/${props.id_student}/${props.id_access}/temporizador`} /> : null
+          }
           {
           Exit
           ? <Redirect to={`/`} /> : null
@@ -241,19 +159,11 @@ export default function HeaderContainer(props) {
                     </a>
                   </li>
                 </ul>
->>>>>>> dcadd127a9922170b58f768a0f2036091a12e9c6
               </li>
             </ul>
-          </li>
-        </ul>
-      </header>
-      <div className="alumnoH-header-base"></div>
-      <SocketStream
-        apiUrl={props.apiUrl}
-        id_access={props.id_access}
-        id_student={props.id_student}
-        socketUrl={props.socketUrl}
-      />
-    </div>
-  );
+            </header>
+            <div className="alumnoH-header-base"></div>
+          <SocketStream apiUrl={props.apiUrl} id_access={props.id_access} id_student={props.id_student} socketUrl={props.socketUrl}/>
+        </div>
+    )
 }
--- src/view/classAndCourse/FormPostSiagie.jsx
+++ src/view/classAndCourse/FormPostSiagie.jsx
@@ -1,16 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class FormPostSiagie extends Component {
-    render() {
-        return (
-            <>
-                <label className="modal-title__controlname">
-                  Subir Excel aqui
-                </label>
-            <input type="file" required name="file" id="excel" onChange={this.handleFileChange} placeholder="Archivo de excel" />
-            <br/>
-            <br/>
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <label className="modal-title__controlname">Subir Excel aqui</label>
+        <input
+          type="file"
+          required
+          name="file"
+          id="excel"
+          onChange={this.handleFileChange}
+          placeholder="Archivo de excel"
+        />
+        <br />
+        <br />
+      </>
+    );
+  }
 }
