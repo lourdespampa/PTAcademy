dba495e2d33ba79ae59de50599cbc9c752ef9899
Miryan Orellana <miryan.orellana@playtecgroup.com>
2020-03-26 09:23:24 -0500

    forms 70%

--- src/view/class/Forms.jsx
+++ src/view/class/Forms.jsx
@@ -1,175 +1,230 @@
 import React, { Component } from "react";
-import NavCourse from "../classAndCourse/NavCourse";
+import axios from "axios";
 export default class Forms extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      idteacher: "",
+      id_class: "",
       valueType: "0",
       questions: [],
-      questions1: [],
-      questions2: [{
-        question : '',
-        answer: ''
-      }],
-      questions3: [],
-      questions4: [],
+      type: "",
+      question: "",
+      answer: "",
+      answer1: "",
+      answer2: "",
+      answer3: "",
+      answer4: "",
+      correctAnswer: ""
     };
   }
 
-  componentDidMount() {}
+  componentDidMount() {
+    console.log(this.props.apiUrl);
+    console.log(this.props.id_class);
+    // "add form": "POST  /v1/api/form/add_form/:id_class    (questions:array)",
+    // "get form": "GET  /v1/api/form/get_form/:id_class",
+  }
   handleChange = e => {
-    console.log(e.target.value);
     this.setState({
       valueType: e.target.value
     });
   };
   saveInputs = e => {
-    console.log(e.target.name, e.target.value);
     switch (this.state.valueType) {
       case "0":
         console.log("no se realiza ninguna accion");
         break;
       case "1":
         // console.log("primera opcion");
-        this.setState({[e.target.name]: [e.target.value]})
+        this.setState({
+          [e.target.name]: e.target.value
+        });
+        setTimeout(() => {
+          console.log("1. pregunta", this.state.question);
+          console.log("2. res1", this.state.answer1);
+          console.log("3. res2", this.state.answer2);
+          console.log("4. res3", this.state.answer3);
+          console.log("5. res4", this.state.answer4);
+          console.log("6. correcta", this.state.correctAnswer);
+        }, 2000);
         break;
       case "2":
-        // console.log("segunda opcion");
         this.setState({
-          questions2: {
-            [e.target.name]: [e.target.value]
-          }
-        })
+          [e.target.name]: e.target.value
+        });
         break;
       case "3":
-        console.log("tercera opcion");
+        this.setState({
+          [e.target.name]: e.target.value
+        });
         break;
       case "4":
         console.log("cuarta opcion");
         break;
       default:
-        console.log('errr')
+        console.log("errr");
         break;
     }
   };
   saveQuestion = () => {
-    console.log("se tiene que guardar la pregunta");
-    console.log(this.state.questions1)
+    switch (this.state.valueType) {
+      case "0":
+        console.log("no se realiza ninguna accion");
+        break;
+      case "1":
+        if (this.state.question !== "") {
+          this.state.questions.push({
+            type: this.state.valueType,
+            pregunta: this.state.question,
+            res1: this.state.answer1,
+            res2: this.state.answer2,
+            res3: this.state.answer3,
+            res4: this.state.answer4,
+            correctAnswer: this.state.correctAnswer
+          });
+        } else {
+          console.log("esta vacio los campos");
+        }
+
+        break;
+      case "2":
+        if (this.state.question !== "") {
+          this.state.questions.push({
+            type: this.state.valueType,
+            pregunta: this.state.question,
+            correctAnswer: this.state.answer
+          });
+        } else {
+          console.log("esta vacio los campos");
+        }
+
+        break;
+      case "3":
+        if (this.state.question !== "") {
+          this.state.questions.push({
+            type: this.state.valueType,
+            pregunta: this.state.question,
+            correctAnswer: this.state.answer
+          });
+        } else {
+          console.log("esta vacio los campos");
+        }
+        break;
+      default:
+    }
+  };
+  handleSubmit = () => {
+    console.log("se tiene que enviar el formulario");
   };
   render() {
     return (
       <>
-        <NavCourse
-          idteacher={localStorage.getItem("id_teacher")}
-          agregarX={"formulario"}
-        ></NavCourse>
-        parte de los formularios
-        <div className="ClassTeacher-main">
-          <h1 className="courseTeacher-title--class">SECCION DE FORMULARIOS</h1>
-          <br></br>
-          <button>AGREGAR FORMULARIOS</button>
-          <form>
-            <select
-              value={this.state.handleChange}
-              onChange={this.handleChange}
-              name="tipo"
-            >
-              <option value="0">seleccionar</option>
-              <option value="1">Para marcar</option>
-              <option value="2">True and false</option>
-              <option value="3">responder preguntar</option>
-              <option value="4">Rellenar</option>
-            </select>
-            {this.state.valueType === "1" ? (
-              <div>
-                <div onChange={this.saveInputs.bind(this)}>
-                  <label>pregunta</label>
-                  <input
-                    type="text"
-                    placeholder="insertar pregunta"
-                    name="question"
-                  />
-                  <br />
-                  <input
-                    type="text"
-                    placeholder="escriba su primera respuesta"
-                    name="answer1"
-                    id=""
-                  />
-                  <input type="radio" value="answer1" name="answer" />
-                  <br />
-                  <input
-                    type="text"
-                    placeholder="escriba su segunda respuesta"
-                    name="answer2"
-                    id=""
-                  />
-                  <input type="radio" value="answer2" name="answer" />
-                  <br />
-                  <input
-                    type="text"
-                    placeholder="escriba su tercera respuesta"
-                    name="answer3"
-                    id=""
-                  />
-                  <input type="radio" value="answer3" name="answer" />
-                  <br />
-                  <input
-                    type="text"
-                    placeholder="escriba su cuarta respuesta"
-                    name="answer4"
-                    id=""
-                  />
-                  <input type="radio" value="answer4" name="answer" />
-                  <br />
-                </div>
-                <button type="button" onClick={this.saveQuestion}>
-                    GUARDAR PREGUNTA
-                  </button>
-              </div>
-            ) : this.state.valueType === "2" ? (
-              <div>
+        <div>seccion donde se van a mostrar los formularios</div>
+        <select
+            value={this.state.handleChange}
+            onChange={this.handleChange}
+            name="tipo"
+          >
+            <option value="0">seleccionar</option>
+            <option value="1">Para marcar</option>
+            <option value="2">True and false</option>
+            <option value="3">responder preguntar</option>
+            <option value="4">Rellenar</option>
+          </select>
+        <form onSubmit={this.handleSubmit}>
+          
+          {this.state.valueType === "1" ? (
+            <div>
+              <div onChange={this.saveInputs.bind(this)}>
+                <label>pregunta</label>
                 <input
                   type="text"
-                  placeholder="insertar preguntar"
-                  onChange={this.saveInputs}
+                  placeholder="insertar pregunta"
                   name="question"
                 />
-                <div onChange={this.saveInputs.bind(this)}>
-                  <input type="radio" value="true" name="answer" id="true" />
-                  <label htmlFor="true">verdadero</label>
-                  <input type="radio" id="false" value="false" name="answer" />
-                  <label htmlFor="false">falso</label>
-                </div>
+                <br />
+                <input
+                  type="text"
+                  placeholder="escriba su primera respuesta"
+                  name="answer1"
+                />
+                <input type="radio" value="answer1" name="correctAnswer" />
+                <br />
+                <input
+                  type="text"
+                  placeholder="escriba su segunda respuesta"
+                  name="answer2"
+                />
+                <input type="radio" value="answer2" name="correctAnswer" />
+                <br />
+                <input
+                  type="text"
+                  placeholder="escriba su tercera respuesta"
+                  name="answer3"
+                />
+                <input type="radio" value="answer3" name="correctAnswer" />
+                <br />
+                <input
+                  type="text"
+                  placeholder="escriba su cuarta respuesta"
+                  name="answer4"
+                />
+                <input type="radio" value="answer4" name="correctAnswer" />
+                <br />
+              </div>
+              <button type="button" onClick={this.saveQuestion}>
+                AÑADIR PREGUNTA
+              </button>
+            </div>
+          ) : this.state.valueType === "2" ? (
+            <div>
+              <input
+                type="text"
+                placeholder="insertar preguntar"
+                onChange={this.saveInputs}
+                name="question"
+              />
+              <div onChange={this.saveInputs.bind(this)}>
+                <input type="radio" value="true" name="answer" id="true" />
+                <label htmlFor="true">verdadero</label>
+                <input type="radio" id="false" value="false" name="answer" />
+                <label htmlFor="false">falso</label>
+              </div>
+              <button type="button" onClick={this.saveQuestion}>
+                AÑADIR PREGUNTA
+              </button>
+            </div>
+          ) : this.state.valueType === "3" ? (
+            <div>
+              pregunta para responder
+              <div>
+                <label>insertar pregunta</label>
+                <input
+                  onChange={this.saveInputs}
+                  name="question"
+                  type="text"
+                  placeholder="insertar preguntar"
+                />
+                <input
+                  onChange={this.saveInputs}
+                  name="answer"
+                  type="text"
+                  placeholder="insertar respuesta"
+                />
                 <button type="button" onClick={this.saveQuestion}>
-                  GUARDAR PREGUNTA
+                  AÑADIR PREGUNTA
                 </button>
               </div>
-            ) : this.state.valueType === "3" ? (
-              <div>
-                pregunta para responder
-                <div>
-                  <label>insertar pregunta</label>
-                  <input
-                    onChange={this.saveInputs}
-                    name="question"
-                    type="text"
-                    placeholder="insertar preguntar"
-                  />
-                  <button type="button" onClick={this.saveQuestion}>
-                    GUARDAR PREGUNTA
-                  </button>
-                </div>
-              </div>
-            ) : (
-              <div>pregunta para rellenar</div>
-            )}
-          </form>
-
-          <div>
-            <div>{/* opcion numero 1  */}</div>
-          </div>
+            </div>
+          ) : (
+            <div>pregunta para rellenar</div>
+          )}
+          <button className="modal-body__button cursos" type="submit">
+            <div className="button-zoom">Agregar Alumno</div>
+          </button>
+        </form>
+        <div>
+          <div>{/* opcion numero 1  */}</div>
         </div>
       </>
     );
